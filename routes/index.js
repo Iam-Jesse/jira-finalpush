566fc5b71755ef1014ef356ee17d58b8a69e0efa
Glitch (jira-addon) <none>
2021-01-07 13:24:30 +0000

    🏀🙏 Checkpoint ./routes/index.js:16744945/12 ./views/issue-view.hbs:16744945/115

--- routes/index.js
+++ routes/index.js
@@ -70,7 +70,7 @@ export default function routes(app, addon) {
             if(req.body.value.includes((eachContent._id).toString())){
               eachContent.status = !eachContent.status;
               eachIssue.save();
-              resultArray.push(eachIssue.toObject());
+              resultArray.push(eachContent.toObject());
             }
           });
         });
